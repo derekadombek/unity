25c9ba776e01917a0595b42d152833e1fb795d69
Derek Dombek <derekdombek@Dereks-MacBook-Pro.local>
2020-10-06 14:59:45 -0600

    wip

--- 3_Boost/Assets/Temp.cs
+++ 3_Boost/Assets/Temp.cs
@@ -7,7 +7,7 @@ public class Temp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("hello");
     }
 
     // Update is called once per frame
